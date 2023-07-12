c0659714a881566d241c21694287737eefd4e8f4
leonardo <leonardotb@id.uff.br>
2023-07-11 21:32:47 -0300

    install springfox and swagger2

--- src/main/java/com/cms/springrestapi/SpringRestApiApplication.java
+++ src/main/java/com/cms/springrestapi/SpringRestApiApplication.java
@@ -2,8 +2,10 @@ package com.cms.springrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringRestApiApplication {
 
 	public static void main(String[] args) {
