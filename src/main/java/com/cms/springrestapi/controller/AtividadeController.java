d8622aeb059a8a4dc36ba95172c2a78941a4191a
Lorenzo <lorenzo.acetii@gmail.com>
2023-07-13 00:23:42 -0300

    Create documentation for atividade, edicao and espaco

--- src/main/java/com/cms/springrestapi/controller/AtividadeController.java
+++ src/main/java/com/cms/springrestapi/controller/AtividadeController.java
@@ -19,8 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cms.springrestapi.model.Atividade;
 import com.cms.springrestapi.service.AtividadeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/atividades")
+@Tag(name = "atividade-api")
 public class AtividadeController {
 
     private final AtividadeService atividadeService;
@@ -30,6 +36,13 @@ public class AtividadeController {
     }
 
     // Obter todas as atividades
+    @Operation(summary = "Busca dados de todas as atividades", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados")
+    })
     @GetMapping
     public ResponseEntity<List<Atividade>> listarAtividades() {
         List<Atividade> atividades = atividadeService.listarAtividades();
@@ -37,6 +50,13 @@ public class AtividadeController {
     }
 
     // Obter detalhes de uma atividade específica
+    @Operation(summary = "Busca dados de um atividade específica", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar busca dos dados")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Atividade> obterAtividade(@PathVariable("id") Long id) {
         Atividade atividade = atividadeService.obterAtividade(id);
@@ -48,6 +68,13 @@ public class AtividadeController {
     }
 
     // Criar uma nova atividade
+    @Operation(summary = "Cria uma nova atividade", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Uma nova atividade foi criada"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao criar atividade")
+    })
     @PostMapping
     public ResponseEntity<Atividade> criarAtividade(@RequestBody Atividade atividade) {
         Atividade atividadeCriada = atividadeService.criarAtividade(atividade);
@@ -55,8 +82,16 @@ public class AtividadeController {
     }
 
     // Atualizar detalhes de uma atividade específica
+    @Operation(summary = "Atualiza detalhes de uma atividade específica", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados atualizados com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao atualizar dados")
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Atividade> atualizarAtividade(@PathVariable("id") Long id, @RequestBody Atividade atividadeAtualizada) {
+    public ResponseEntity<Atividade> atualizarAtividade(@PathVariable("id") Long id,
+            @RequestBody Atividade atividadeAtualizada) {
         Atividade atividade = atividadeService.obterAtividade(id);
         if (atividade != null) {
             atividade.setNome(atividadeAtualizada.getNome());
@@ -74,8 +109,16 @@ public class AtividadeController {
     }
 
     // Atualizar detalhes parciais de uma atividade específica
+    @Operation(summary = "Atualiza detalhes parciais de uma atividade específica", method = "PATCH")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados atualizados com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao atualizar dados")
+    })
     @PatchMapping("/{id}")
-    public ResponseEntity<Atividade> atualizarCampoAtividade(@PathVariable("id") Long id, @RequestBody Map<String, Object> updates) {
+    public ResponseEntity<Atividade> atualizarCampoAtividade(@PathVariable("id") Long id,
+            @RequestBody Map<String, Object> updates) {
         Atividade atividade = atividadeService.obterAtividade(id);
         if (atividade != null) {
             ModelMapper modelMapper = new ModelMapper();
@@ -91,6 +134,13 @@ public class AtividadeController {
     }
 
     // Excluir uma atividade específica
+    @Operation(summary = "Exclui uma atividade", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Atividade excluída com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados de requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao excluir atividade")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> excluirAtividade(@PathVariable("id") Long id) {
         boolean removido = atividadeService.excluirAtividade(id);
